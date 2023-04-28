69d45b383bfc72e503fe4dab9a8fe3684d37830e
Miguel Angel Muñoz Aristizabal <mimunoza@unal.edu.co>
2023-04-27 21:53:31 -0500

    cambio de metodos de plataformas por un diccionario con los formatos, solución del problema de fechas, aprobado, txt con pdfs atipicos

--- clasificador_pdfs.py
+++ clasificador_pdfs.py
@@ -1,14 +1,11 @@
 import PyPDF2
 import re
-from datetime import datetime
+import datetime
+import locale
+from dateutil.parser import parse
 
 # # Abrir el archivo PDF en modo lectura binaria
 # archivo_pdf = open("..\ejemUClaro.pdf", 'rb')  
-
-# iden_ssff = ('Hace constar que' and 'ha completado el curso')
-# iden_carso = ('para acreditar que' and 'completó y aprobó los estudios de')
-# iden_uclaro = ('UNIVERSIDAD CLARO certifica que' and 'Ha superado cumpliendo con los requisitos académicos exigidos por el curso' ) 
-# iden_coursera = 'ofrecido a través de Courseracompletó con éxito' and ' Coursera confirmó la identidad de esta persona'
 
 # cursos_obligatorios = ['Protección de datos personales', 'Código de Conducta', 'Curso UCC', 'Curso Habilidades UCC',
 #                        'Ransomware', 'Incapacidades','Lavado de Dinero', 'Convivencia Saludable', 'Curso UCC','Curso Habilidades UCC',
@@ -18,111 +15,121 @@ from datetime import datetime
 #                        'Agilidad 2.0', 'Decálogo del Líder Ágil' , 'Comunicar es la Onda']                     
 
 
-def buscar_en_pdf(texto_pag, inicio, final):
-    if isinstance(inicio, int) :
-        index_inicio = inicio
-    else:
-        index_inicio = texto_pag.find(inicio) + len(inicio)
-
-    if isinstance(final, int) :
-        index_final = final
-    else:
-        index_final = texto_pag.find(final) 
-
-    return texto_pag[index_inicio:index_final]
-
-
-def SSFF(texto_pag):
-    #Nombre certificado
-    inicio_nombre = 'constar que'
-    final_nombre = 'ha completado'
-    nombre_certificado = buscar_en_pdf(texto_pag, inicio_nombre, final_nombre)
-
-    #Fecha
-    inicio_fecha = -10
-    final_fecha = -1
-    fecha_certificado = buscar_en_pdf(texto_pag, inicio_fecha, final_fecha)
-    #Certificado
-
-    inicio_certificado = 'el curso'
-    index_final_certificado  = inicio_fecha
-    certificado = buscar_en_pdf(texto_pag, inicio_certificado,index_final_certificado )
-
-    return nombre_certificado, certificado,  fecha_certificado 
-
-def CARSO(texto_pag):
-    #Nombre certificado
-    inicio_nombre = 'acreditar que'
-    final_nombre = 'completó y '
-
-    nombre_certificado = buscar_en_pdf(texto_pag, inicio_nombre, final_nombre)
-
-    #Fecha
-    inicio_fecha = 'México a '
-    final_fecha = 'Certificado de '
-    fecha_certificado = buscar_en_pdf(texto_pag, inicio_fecha, final_fecha)
-    #Certificado
-
-    inicio_certificado = 'estudios de'
-    index_final_certificado  = 'presentando a la '
-    certificado = buscar_en_pdf(texto_pag, inicio_certificado,index_final_certificado )
-
-    return nombre_certificado, certificado,  fecha_certificado 
-
-def UCLARO(texto_pag):
-    #Nombre certificado
-    inicio_nombre = 'certifica que'
-    final_nombre = 'Ha superado'
-
-    nombre_certificado = buscar_en_pdf(texto_pag, inicio_nombre, final_nombre)
-
-    #Fecha
-    inicio_fecha = 'finalización'
-    final_fecha = -1
-    fecha_certificado = buscar_en_pdf(texto_pag, inicio_fecha, final_fecha)
-    #Certificado
-
-    inicio_certificado = 'por el curso'
-    index_final_certificado  = 'Fecha'
-    certificado = buscar_en_pdf(texto_pag, inicio_certificado,index_final_certificado )
-
-    return nombre_certificado, certificado,  fecha_certificado 
 
 def extraer_informacion(dir_pdf):
-    archivo_pdf = open(dir_pdf, 'rb') 
     
-    iden_ssff = ('Hace constar que' and 'ha completado el curso')
-    iden_carso = ('para acreditar que' and 'completó y aprobó los estudios de')
-    iden_uclaro = ('UNIVERSIDAD CLARO certifica que' and 'Ha superado cumpliendo con los requisitos académicos exigidos por el curso' ) 
-    iden_coursera = 'ofrecido a través de Courseracompletó con éxito' and ' Coursera confirmó la identidad de esta persona'
+    archivo_pdf = open(dir_pdf, 'rb') 
+
+    formatos_cursos = {}
+    formatos_cursos["ssff"] = {"inicioNombre":'Hace constar que', 
+                               "finalNombre":'ha completado el curso',
+                               "inicioFecha": -10, 
+                               "finalFecha": -1,
+                               "inicioCertificado": 'el curso', 
+                               "finalCertificado": -10}
+    
+    formatos_cursos["carso"] = {"inicioNombre": 'para acreditar que' ,
+                                "finalNombre": 'completó y aprobó los estudios de',
+                                "inicioFecha" : 'México a ',
+                                "finalFecha" : 'Certificado de ',
+                                "inicioCertificado" : 'estudios de ',
+                                "finalCertificado"  : 'presentando a la '}
+    
+    formatos_cursos["uclaro"] = {"inicioNombre": 'UNIVERSIDAD CLARO certifica que' ,
+                                 "finalNombre": 'Ha superado cumpliendo con los requisitos académicos exigidos por el curso',
+                                 "inicioFecha" : 'finalización', "finalFecha" : -1,
+                                 "inicioCertificado" :'por el curso',
+                                 "finalCertificado": 'Fecha'}
+    
+    formatos_cursos["coursera"] = {"inicioNombre": 'ofrecido a través de Courseracompletó con éxito' ,
+                                   "finalNombre": 'Coursera confirmó la identidad de esta persona'}
+    
     
     pdf_reader = PyPDF2.PdfFileReader(archivo_pdf)
     pagina = pdf_reader.getPage(0)
     texto_pag = pagina.extractText()
-    #print(pagina.extractText())
-
-    if iden_ssff in texto_pag:
-        a, b, c = SSFF(texto_pag)
-        return (a, b, c)
-    elif iden_carso in texto_pag :
-        a, b, c = CARSO(texto_pag)
-        return (a, b, c)
-    elif iden_uclaro in texto_pag :
-        a, b, c = UCLARO(texto_pag)
-        return (a, b, c)
-    elif iden_coursera in texto_pag :
-        return None, None, None
+    
+    plataforma = "sin identificar"
+    
+    ## la clave representa la plataforma donde se hizo el certificado
+    for clave, valor in formatos_cursos.items():
+        if valor["inicioNombre"] in texto_pag: plataforma = clave
+    
+    ##una vez se extrae y valida la plataforma, se busca los parametros
+    if plataforma == "coursera" or plataforma == "sin identificar" :
+        print("este certificado no cumple con los requisitos")
+        nombre, certificado, fecha, aprobado, talentos = None,None,None,None,None
     else:
-       return None, None, None
- 
+        nombre, certificado, fecha, aprobado, talentos = buscarParametrosDeRegistro(plataforma, texto_pag, formatos_cursos)
+    
+    return nombre, certificado, fecha, plataforma, aprobado, talentos
+
+
+def buscarParametrosDeRegistro(plataforma, texto_pag, formatos_cursos):
+    
+    
+    nombre = buscar_en_pdf(texto_pag, formatos_cursos[plataforma]["inicioNombre"],
+                           formatos_cursos[plataforma]["finalNombre"])
+    
+    certificado = buscar_en_pdf(texto_pag, formatos_cursos[plataforma]["inicioCertificado"],
+                           formatos_cursos[plataforma]["finalCertificado"])
+    
+    fechaSinFormato = buscar_en_pdf(texto_pag, formatos_cursos[plataforma]["inicioFecha"],
+                           formatos_cursos[plataforma]["finalFecha"])
+    
+    fechaConFormato = formatearFecha(fechaSinFormato, plataforma)
+
+    fechaLimite = parse("01/04/2022", dayfirst=True)
+    fechaLimite = fechaLimite.strftime("%d/%m/%Y")
+
+    aprobado = fechaLimite < fechaConFormato
+    
+    return nombre,certificado, fechaConFormato, aprobado,None
+
+
+def buscar_en_pdf(texto_pag, inicio, final):
+    
+    if isinstance(inicio, int) : index_inicio = inicio
+    else: index_inicio = texto_pag.find(inicio) + len(inicio)
+
+    if isinstance(final, int) :  index_final = final
+    else: index_final = texto_pag.find(final) 
+
+    return texto_pag[index_inicio:index_final]
+
+def formatearFecha(fechaSinFormato, plataforma):
+    
+    try:
+
+        ##en caso de ser carso, convierte texto a fecha
+        if(plataforma == "carso"): 
+            # Establecer el idioma en español
+            locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+            # Fecha en formato string
+            fecha = fechaSinFormato[:-1]
+            # Convertir a objeto date
+            fechaConFormato = datetime.datetime.strptime(fecha, '%d de %B del %Y').date()
+            fechaConFormato = fechaConFormato.strftime("%d/%m/%Y")
+            
+        else:
+            ##en caso de cualquier otra plataforma solo formatea la fecha
+            fecha = parse(fechaSinFormato, dayfirst=True)
+            fechaConFormato = fecha.strftime("%d/%m/%Y")
+        
+    except:
+        ##en caso de error, es porque la fecha no tiene formato valido
+        print("problema formateando la fecha")
+        fechaConFormato = fechaSinFormato
+    
+    return fechaConFormato
+
 
 if __name__ == '__main__':    
-    var1 = "C:\\Users\\Santiago\\Documents\\pdfscertificadosclaro" 
-    var2 ="006fedd1-3d70-4f5b-8e4e-cded1e9984c7.pdf"
+    var1 = "C:\\Users\\migue\\OneDrive\\Documentos\\pdfscertificadosclaro"  
+    var2 ="178708f8-b115-4bd1-af0e-9d713e590936.pdf"
     x = "{}/{}".format(var1,var2)
-
-    a,b, c = extraer_informacion(x)
-    print(c)
+    nombre, certificado, fecha, plataforma, aprobado, talentos =extraer_informacion(x)
+    print(fecha)
 
 
 
--- recorrer_directorio.py
+++ recorrer_directorio.py
@@ -4,29 +4,50 @@ import clasificador_pdfs
 
 # Comprobar si el archivo ya existe
 if os.path.isfile("certificados_validados.xlsx"):
-    print("El archivo ya existe.")
     df = pd.read_excel("certificados_validados.xlsx")
+    
 else:
     # Si no existe, crear el archivo
     df = pd.DataFrame(columns=["id_pdf", "empleado","certificado", "fecha", "plataforma", "aprobado", "talentos"])
     df.to_excel("certificados_validados.xlsx", index=False)
     print("Se ha creado el archivo 'certificados_validados.xlsx'.")
+    
 
-directorio = "C:\\Users\\Santiago\\Documents\\pdfscertificadosclaro"  
-
+directorio = "C:\\Users\\migue\\OneDrive\\Documentos\\pdfscertificadosclaro"  
 lis = []
 var =0
-for archivo in os.listdir(directorio):
-    ruta_archivo = os.path.join(directorio, archivo)
-    if os.path.isfile(ruta_archivo):
-        # Aquí puedes hacer lo que quieras con cada archivo
-        if archivo != "54b08d0b-9fa1-47c3-afef-e4b2362101a7.pdf":
-            if (not df['id_pdf'].isin([archivo]).any()):
-                #lis.append(archivo)
-                nombre, certificado, fecha = clasificador_pdfs.extraer_informacion("{}/{}".format(directorio,archivo))
-                nuevo_valor = pd.DataFrame({"id_pdf":[archivo], "empleado":[nombre],"certificado":[certificado],
-                                             "fecha":[fecha], "plataforma":[None], "aprobado":[None], "talentos":[None]})
-                df = pd.concat([df,nuevo_valor] )
+listaDocumentosRaros = [];
 
-#print(df)
+## se recorre cada archivo en la carpeta del directorio
+for archivo in os.listdir(directorio):
+    
+    ##genera la ruta completa del archivo
+    ruta_archivo = os.path.join(directorio, archivo) 
+    ##valida que el archivo existe
+    if os.path.isfile(ruta_archivo):
+        try:
+            ##se valida si hay algún archivo repetido
+            # if (not df['id_pdf'].isin([archivo]).any()):
+                ##se extrae la información del certificado
+                nombre, certificado, fecha, plataforma, aprobado, talentos = clasificador_pdfs.extraer_informacion("{}/{}".format(directorio,archivo))
+                ##se crea un nuevo registro a partir de la información
+                nuevo_valor = pd.DataFrame({"id_pdf":[archivo], "empleado":[nombre],"certificado":[certificado],
+                                            "fecha":[fecha], "plataforma":[plataforma], "aprobado":[None], "talentos":[None]})
+                ##se guarda el registro en el objeto df
+                df = pd.concat([df,nuevo_valor] )    
+                
+        except Exception as e:
+            print("\n Se ha producido un error:", e)       
+            print("\n problema de lectura con el archivo: ",ruta_archivo)
+            listaDocumentosRaros.append(ruta_archivo)
+                        
 df.to_excel("certificados_validados.xlsx", index=False)
+
+
+# Abrir un archivo en modo escritura ('w')
+with open('documentosRaros.txt', 'w') as archivo:
+    # Convertir la lista en una cadena de texto separada por comas
+    lista_como_texto = "\n".join(map(str, listaDocumentosRaros))
+    # Escribir la cadena de texto en el archivo
+    archivo.write(lista_como_texto)
+
